4f387deaacf2a072b8fd0bbc7df2d6369f16ab27
lian <lianrinofernandez@gmail.com>
2023-05-26 16:01:08 +0800

    snake_case corrections

--- app/Models/PostView.php
+++ app/Models/PostView.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PostViews extends Model
+class PostView extends Model
 {
     use HasFactory;
 
