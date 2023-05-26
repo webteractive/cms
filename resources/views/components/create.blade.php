6d7df77ccff8bd1211b720055d4b55aea1be7877
Aldrin Paulo Wabingga <aldrinwabingga@gmail.com>
2023-05-26 16:34:17 +0800

    WIP

--- resources/views/components/create.blade.php
+++ resources/views/components/create.blade.php
@@ -80,7 +80,7 @@
                         class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tutorial</a>
                 </li>
                 <li>
-                    <a href="Video"
+                    <a href="#"
                         class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Video</a>
                 </li>
             </ul>
